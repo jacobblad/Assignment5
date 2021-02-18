3eff99e90cff7a3b5f85ed8efa1ca4548ae1f7d6
jacobblad <66271471+jacobblad@users.noreply.github.com>
2021-02-17 23:38:09 -0700

    Add project files.

---
+++ Models/EFBookStoreRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5.Models
+{
+    public class EFBookStoreRepository : IBookStoreRepository
+    {
+        private BookStoreDbContext _context;
+
+        //Constructor
+        public EFBookStoreRepository (BookStoreDbContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<Project> Projects => _context.Projects;
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5.Models
+{
+    public class SeedData
+    {
+        public static void EnsurePopulated (IApplicationBuilder application)
+        {
+            BookStoreDbContext context = application.ApplicationServices.
+                CreateScope().ServiceProvider.GetRequiredService<BookStoreDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            if (!context.Projects.Any())
+            {
+                context.Projects.AddRange(
+
+                    new Project
+                    {
+                        Title = "Les Miserables",
+                        Author = "Victor Hugo",
+                        Publisher = "Signet",
+                        ISBN = "978-0451419429",
+                        Category = "Fiction, Classic",
+                        Price = 9.95F
+                    },
+
+                    new Project
+                    {
+                        Title = "The Virgin Way",
+                        Author = "Richard Branson",
+                        Publisher = "Portfolio",
+                        ISBN = "978-1591847984",
+                        Category = "Non-Fiction, Business",
+                        Price = 29.16F
+                    }
+
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
